044f28978bf27da51844d47955f0a053c7aba892
Vishal Biradar <vishalbiradar205@gmail.com>
2026-04-19 19:29:31 +0000

    validate employee salary positive

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,5 @@
 class Employee < ApplicationRecord
   validates :full_name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :salary, presence: true
+  validates :salary, presence: true, numericality: { greater_than: 0 }
 end
