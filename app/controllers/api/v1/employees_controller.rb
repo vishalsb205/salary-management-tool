64ddd564babbc475c26ca1de5a12acdd8263196a
Vishal Biradar <vishalbiradar205@gmail.com>
2026-04-19 19:57:09 +0000

    add employee create endpoint

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -4,6 +4,17 @@ module Api
       def index
         render json: Employee.all
       end
+
+      def create
+        employee = Employee.create!(employee_params)
+        render json: employee, status: :created
+      end
+
+      private
+
+      def employee_params
+        params.require(:employee).permit(:full_name, :email, :salary, :job_title, :department, :country, :active)
+      end
     end
   end
 end
