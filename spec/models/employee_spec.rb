33d82b64c06df88c8abe2447b58a77fe82c34a6b
Vishal Biradar <vishalbiradar205@gmail.com>
2026-04-19 19:12:52 +0000

    add failing employee spec for email presence

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -13,5 +13,17 @@ RSpec.describe Employee, type: :model do
       expect(employee).not_to be_valid
       expect(employee.errors[:full_name]).to include("can't be blank")
     end
+
+    it "is invalid without an email" do
+      employee = Employee.new(
+        full_name: "Vishal Sharma",
+        job_title: "HR Manager",
+        country: "India",
+        salary: 75000
+      )
+
+      expect(employee).not_to be_valid
+      expect(employee.errors[:email]).to include("can't be blank")
+    end
   end
 end
