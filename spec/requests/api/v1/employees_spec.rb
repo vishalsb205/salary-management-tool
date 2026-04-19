076210f69b761dd3c9829a4f366d16752e4c0adc
Vishal Biradar <vishalbiradar205@gmail.com>
2026-04-19 19:52:26 +0000

    add validation spec for returning employees from index

--- spec/requests/api/v1/employees_spec.rb
+++ spec/requests/api/v1/employees_spec.rb
@@ -8,5 +8,33 @@ RSpec.describe "Api::V1::Employees", type: :request do
       expect(response).to have_http_status(:ok)
       expect(JSON.parse(response.body)).to eq([])
     end
+
+    it "returns employees" do
+      employee = Employee.create!(
+        full_name: "Vishal Sharma",
+        email: "vishal@example.com",
+        salary: 75000
+      )
+
+      get "/api/v1/employees"
+
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)).to eq(
+        [
+          {
+            "id" => employee.id,
+            "full_name" => "Vishal Sharma",
+            "email" => "vishal@example.com",
+            "salary" => 75000,
+            "job_title" => nil,
+            "department" => nil,
+            "country" => nil,
+            "active" => true,
+            "created_at" => employee.created_at.as_json,
+            "updated_at" => employee.updated_at.as_json
+          }
+        ]
+      )
+    end
   end
 end
