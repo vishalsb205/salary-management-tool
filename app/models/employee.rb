620e2a8c3177d7ae2340658a9247d74fc95c63a7
Vishal Biradar <vishalbiradar205@gmail.com>
2026-04-19 19:37:01 +0000

    validate employee email format

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,8 @@
 class Employee < ApplicationRecord
   validates :full_name, presence: true
-  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :salary, presence: true, numericality: { greater_than: 0 }
 end
