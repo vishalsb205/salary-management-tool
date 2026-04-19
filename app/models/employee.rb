ad19a9b15b3d65125014c6baa1aca42c0db91e7e
Vishal Biradar <vishalbiradar205@gmail.com>
2026-04-19 19:25:17 +0000

    remove redundant full_name attribute declaration

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,4 @@
 class Employee < ApplicationRecord
-  attribute :full_name, :string
-
   validates :full_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :salary, presence: true
