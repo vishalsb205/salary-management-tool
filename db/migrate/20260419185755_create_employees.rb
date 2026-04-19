c6c26a0cabb2b9b00f7c7c9903ab7b401d3ac471
Vishal Biradar <vishalbiradar205@gmail.com>
2026-04-19 19:07:22 +0000

    add employees table with core required constraints

--- db/migrate/20260419185755_create_employees.rb
+++ db/migrate/20260419185755_create_employees.rb
@@ -1,13 +1,13 @@
 class CreateEmployees < ActiveRecord::Migration[7.1]
   def change
     create_table :employees do |t|
-      t.string :full_name
-      t.string :email
+      t.string :full_name, null: false
+      t.string :email, null: false
       t.string :job_title
-      t.string :country
-      t.integer :salary
       t.string :department
-      t.boolean :active
+      t.string :country
+      t.integer :salary, null: false
+      t.boolean :active, null: false, default: true
 
       t.timestamps
     end
