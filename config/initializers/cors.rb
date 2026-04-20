111f396128ed4fd77ac01d35cb13da63016cb2f7
Vishal Biradar <vishalbiradar205@gmail.com>
2026-04-20 19:45:51 +0000

    fix stable ordering and cors for deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("FRONTEND_URL", "*")
+    origins(
+      "https://hr-portal.vercel.app",
+      /https:\/\/hr-portal-.*\.vercel\.app/
+    )
 
     resource "*",
       headers: :any,
